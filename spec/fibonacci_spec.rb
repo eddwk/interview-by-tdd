8f55c2a9f55fcdeff5be5bc84adb8b7e20b7820c
Edward Kerr <edd.kerr@smashingboxes.com>
2018-09-28 15:38:42 -0400

    refactor tests

--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -2,58 +2,38 @@ require 'fibonacci'
 
 describe Fibonacci do
 
-
-  describe "iterative" do
+  shared_examples_for :a_fibonacci_sequence do |method|
     it "should return 0 if 0 is input" do
-      expect(subject.iterative(0)).to eq(0)
+      expect(subject.send(method, 0)).to eq(0)
     end
 
     it "should return 1 if 1 is input" do
-      expect(subject.iterative(1)).to eq(1)
-      expect(subject.iterative(2)).to eq(1)
+      expect(subject.send(method, 1)).to eq(1)
+      expect(subject.send(method, 2)).to eq(1)
     end
 
     it "should return 2 if 3 is input" do
-      expect(subject.iterative(3)).to eq(2)
+      expect(subject.send(method, 3)).to eq(2)
     end
 
     it "should return 3 if 4 is input" do
-      expect(subject.iterative(3)).to eq(2)
+      expect(subject.send(method, 3)).to eq(2)
     end
 
     it "should return 5 if 5 is input" do
-      expect(subject.iterative(5)).to eq(5)
+      expect(subject.send(method, 5)).to eq(5)
     end
 
     it "should return 6 if 8 is input" do
-      expect(subject.iterative(6)).to eq(8)
+      expect(subject.send(method, 6)).to eq(8)
     end
   end
 
+  describe "iterative" do
+    it_behaves_like(:a_fibonacci_sequence, :iterative)
+  end
+
   describe "recursive" do
-    it "should return 0 if 0 is input" do
-      expect(subject.recursive(0)).to eq(0)
-    end
-
-    it "should return 1 if 1 is input" do
-      expect(subject.recursive(1)).to eq(1)
-      expect(subject.recursive(2)).to eq(1)
-    end
-
-    it "should return 2 if 3 is input" do
-      expect(subject.recursive(3)).to eq(2)
-    end
-
-    it "should return 3 if 4 is input" do
-      expect(subject.recursive(3)).to eq(2)
-    end
-
-    it "should return 5 if 5 is input" do
-      expect(subject.recursive(5)).to eq(5)
-    end
-
-    it "should return 6 if 8 is input" do
-      expect(subject.recursive(6)).to eq(8)
-    end
+    it_behaves_like(:a_fibonacci_sequence, :recursive)
   end
 end
