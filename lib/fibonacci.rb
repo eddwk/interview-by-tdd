880f2d489b20b549867cd84d167d28ced20c43f2
Edward Kerr <edd.kerr@smashingboxes.com>
2018-09-28 15:26:13 -0400

    add code to pass tests

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,7 +1,17 @@
 class Fibonacci
-  def iterative
+  def iterative(num)
+    previous = 0
+    current = 1
+
+    num.times do
+      temp = previous
+      previous = current
+      current = temp + previous
+    end
+
+    previous
   end
-  
+
   def recursive
   end
 end
