9b1dda8aa9a8a673cddf628905fd4088e308823c
Edward Kerr <edd.kerr@smashingboxes.com>
2018-09-28 15:24:35 -0400

    add some test cases

--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -1,21 +1,42 @@
 require 'fibonacci'
 
 describe Fibonacci do
+
+
   describe "iterative" do
-    xit("should return 3 if 2 is input") do
+    it "should return 0 if 0 is input" do
+      expect(subject.iterative(0)).to eq(0)
     end
-    xit("should return 1 if 1 is input") do
+
+    it "should return 1 if 1 is input" do
+      expect(subject.iterative(1)).to eq(1)
+      expect(subject.iterative(2)).to eq(1)
     end
-    xit("should return n! for any n") do
+
+    it "should return 2 if 3 is input" do
+      expect(subject.iterative(3)).to eq(2)
+    end
+
+    it "should return 3 if 4 is input" do
+      expect(subject.iterative(3)).to eq(2)
+    end
+
+    it "should return 5 if 5 is input" do
+      expect(subject.iterative(5)).to eq(5)
+    end
+
+    it "should return 6 if 8 is input" do
+      expect(subject.iterative(6)).to eq(8)
     end
   end
+
   describe "recursive" do
-    it("should return 3 if 2 is input") do
-      expect(true).to be false
+    it "should return 3 if 2 is input" do
+      # expect(true).to be false
     end
-    xit("should return 1 if 1 is input") do
+    xit "should return 1 if 1 is input" do
     end
-    xit("should return n! for any n") do
+    xit "should return n! for any n" do
     end
   end
 end
